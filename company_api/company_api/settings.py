06caae28f4b4c903d815511af134e4c24577930f
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-12-24 14:01:48 +0300

    local launch

--- company_api/company_api/settings.py
+++ company_api/company_api/settings.py
@@ -59,12 +59,8 @@ WSGI_APPLICATION = 'company_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', ''),
-        'NAME': os.getenv('POSTGRES_DB', ''),
-        'USER': os.getenv('POSTGRES_USER', ''),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', '')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
